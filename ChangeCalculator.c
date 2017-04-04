8c47126117958a9d80dd69c7389b1a2483658467
LajamerrMittesdine <danieljmerritt4394@gmail.com>
2017-04-04 03:40:55 -0400

    Comment the code

--- ChangeCalculator.c
+++ ChangeCalculator.c
@@ -5,14 +5,30 @@
 void calculateChange(int *c, int amount, int *increment);
  
 int main(void){
+    //input[0] = Paid Dollars, input[1] = Paid Cents 
+    //input[2] = Owe Dollars,  input[3] = Owe Cents
     int input[4] = {0,0,0,0};
-    int a[12] = {0};
+ 
+    //a[0] = Number of $100 bills, a[1] = Number of $50 bills
+    //a[2] = Number of $20 bills,  a[3] = Number of $10 bills
+    //a[4] = Number of $5 bills,   a[5] = Number of $2 bills
+    //a[6] = Number of $1 bills,   a[7] = Number of $0.50 coins
+    //a[8] = Number of $0.25 coins,a[9] = Number of $0.10 coins
+    //a[10]= Number of $0.05 coins,a[11]= Number of $0.01 coins
+    int a[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
     const float b[12] = {10000,5000,2000,1000,500,200,100,50,25,10,5,1};
  
     //Get the user input values
+    //Format is amount the user paid and the amount that is actually owed
+    //2135.35 1454.86 gets broken up into 2135 35 1454 86
     scanf("%d.%d %d.%d",&input[0],&input[1],&input[2],&input[3]);
  
     //Calculate the difference
+    //2135.35 - 1454.86 = 680.49
+    //Covert the dollars to cents
+    //((2135 * 100) + 35) = 213535 cents
+    //((1454 * 100) + 86) = 145486 cents
+    //213535 - 145486     =  68049 cents
     input[0] = (((input[0] * 100) + input[1]) - ((input[2] * 100) + input[3]));
     
     //debug value only
@@ -22,6 +38,7 @@ int main(void){
     for(int i = 0, n = (sizeof(a)/sizeof(a[0]))-1; i<=n; i++){
         calculateChange(&input[0],b[i],&a[i]);
     }
+    
     //Verify the output
     //Debug Message Only
     printf("The change is $%.2f and we calculated $%.2f\n",(debugValue)/100.00,
