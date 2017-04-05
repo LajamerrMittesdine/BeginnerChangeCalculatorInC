cb6418448c825d6217d17443bfb2211283a6cd8d
LajamerrMittesdine <danieljmerritt4394@gmail.com>
2017-04-05 16:52:47 -0400

    Got rid of wordCount variable.

--- ChangeCalculator.c
+++ ChangeCalculator.c
@@ -1,29 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-void calculateChange(int *change, int denominations);
+void calculateChange(unsigned int *change, int denominations);
+int countWords(FILE *f);
  
 int main(void){
-    int change = 0;
-    int tender[2] = {0,0};
-    int bill[2]  = {0,0};
-    const int denominations[12] = {10000,5000,2000,1000,500,200,100,50,25,10,5,1};
- 
-    while (!feof(stdin)){
-		if (scanf("%d.%d %d.%d",&tender[0],&tender[1],&bill[0],&bill[1]) == 4) break;
- 	}
-    change = (((tender[0] * 100) + tender[1]) - ((bill[0] * 100) + bill[1]));
-    for (size_t i = 0; i < sizeof denominations / sizeof *denominations; ++i){
-        calculateChange(&change,denominations[i]);
+    unsigned int change = 0;
+    unsigned int tender[2] = {0,0};
+    unsigned int bill[2]  = {0,0};
+
+    FILE *myFiles;
+    
+    //Get the number of lines of the file
+    myFiles = fopen("denominations.txt", "r");
+    const int size = countWords(myFiles);
+    fclose (myFiles);
+    //Set the array size of denominations to the number of lines in the file.
+    unsigned int denominations[size];
+    
+    myFiles = fopen("denominations.txt", "r");
+    
+    if (myFiles == NULL){
+        printf("Can't open file for reading.\n");
     }
- 
+    else{
+    	for(int i = 0; i < sizeof(denominations)/sizeof(*denominations); ++i){
+    		fscanf(myFiles, "%u", &denominations[i]);
+    	}
+        fclose(myFiles);
+        while (!feof(stdin)){
+		    if (scanf("%u.%u %u.%u",&tender[0],&tender[1],&bill[0],&bill[1]) == 4) break;
+ 	    }
+ 	    
+ 	    //Get the change total in cents
+        change = (((tender[0] * 100) + tender[1]) - ((bill[0] * 100) + bill[1]));
+    
+        for (int i = 0; i < sizeof(denominations)/sizeof(*denominations); ++i){
+            calculateChange(&change,denominations[i]);
+        }
+    }
     return 0;
 }
+
+int countWords(FILE *f){
+   int count = 0;
+   char ch;
+   while ((ch = fgetc(f)) != EOF){
+       if (ch == '\n')
+           count++;
+   }
+   return ++count;
+}
  
-void calculateChange(int *change, int denominations){
+void calculateChange(unsigned int *change, int denominations){
     unsigned int count = *change / denominations;
     *change = *change - denominations*count;
     if(count > 0){
-        printf("%d x $%d.%02d\n",count, denominations/100, denominations%100);
+        printf("%u x $%u.%02u\n",count, denominations/100, denominations%100);
     }
 }
