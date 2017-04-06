e7233874068d2c65a29873e93a833d71ae02c7fd
LajamerrMittesdine <danieljmerritt4394@gmail.com>
2017-04-05 23:45:35 -0400

    Added error checking

--- ChangeCalculator.c
+++ ChangeCalculator.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
  
 void calculateChange(unsigned int *change, unsigned int denominations);
-unsigned int countWords(FILE *f);
+unsigned int getSizeOfDenominations(FILE *f);
 
 int main(void){
     FILE *myFiles;
@@ -10,16 +10,19 @@ int main(void){
     myFiles = fopen("denominations.txt", "r");
     
     if (myFiles == NULL){
-        printf("Can't open denominations file for reading.\n");
+        printf("./denominations.txt either does not exist or can't be opened for reading.\n");
         return 1;
     }
     else{
-       unsigned int denominations[countWords(myFiles)];
        unsigned int change = 0;
        unsigned int tender[2] = {0,0};
        unsigned int bill[2]  = {0,0};
-       fclose (myFiles);
-       myFiles = fopen("denominations.txt", "r");
+       unsigned int denominations[getSizeOfDenominations(myFiles)];
+       
+       if(sizeof(denominations)/sizeof(*denominations) == 0){
+           printf("./denominations.txt was empty.\n");
+           return 2;
+       }
        
        for(size_t i = 0; i < sizeof(denominations)/sizeof(*denominations); ++i){
            fscanf(myFiles, "%u", &denominations[i]);
@@ -36,19 +39,42 @@ int main(void){
        for (size_t i = 0; i < sizeof(denominations)/sizeof(*denominations); ++i){
            calculateChange(&change,denominations[i]);
        }
+       if(change > 0){
+           printf("The total amount of change wasn't completely divisible by the denominations set.\nYour total change left over is $%u.%02u\n",change/100,change%100);
+       }
     }
     return 0;
 }
 
-unsigned int countWords(FILE *f){
+unsigned int getSizeOfDenominations(FILE *f){
    unsigned int count = 0;
    char ch;
-   while ((ch = fgetc(f)) != EOF){
-       if (ch == '\n'){
-           count++;
+   if (f == NULL){
+       fclose(f);
+       return 0;
+   }
+   else{
+       while(!feof(f)){
+           ch = fgetc(f);
+           if(ch == '\n'){
+               count++;
+           }
+       }
+       if(count==0){
+           if(ftell(f) > 0){
+               rewind(f);
+               return 1;
+           }
+           else{
+               rewind(f);
+               return 0;
+           }
+       }
+       else{
+           rewind(f);
+           return ++count;
        }
    }
-   return ++count;
 }
  
 void calculateChange(unsigned int *change, unsigned int denominations){
