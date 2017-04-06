e070591c4fe58ccd5e1965d4a451e1f47cfa8dca
LajamerrMittesdine <danieljmerritt4394@gmail.com>
2017-04-05 21:12:26 -0400

    Formatting changes.

--- ChangeCalculator.c
+++ ChangeCalculator.c
@@ -5,38 +5,37 @@ void calculateChange(unsigned int *change, unsigned int denominations);
 unsigned int countWords(FILE *f);
 
 int main(void){
-    unsigned int change = 0;
-    unsigned int tender[2] = {0,0};
-    unsigned int bill[2]  = {0,0};
-
     FILE *myFiles;
     
     myFiles = fopen("denominations.txt", "r");
     
     if (myFiles == NULL){
-        printf("Can't open file for reading.\n");
+        printf("Can't open denominations file for reading.\n");
+        return 1;
     }
     else{
-        const unsigned int size = countWords(myFiles);
-        fclose (myFiles);
-        myFiles = fopen("denominations.txt", "r");
-        unsigned int denominations[size];
-
-    	for(unsigned int i = 0; i < sizeof(denominations)/sizeof(*denominations); ++i){
-    		fscanf(myFiles, "%u", &denominations[i]);
-    	}
-        fclose(myFiles);
-
-        while (!feof(stdin)){
-		if (scanf("%u.%u %u.%u",&tender[0],&tender[1],&bill[0],&bill[1]) == 4) break;
-	}
- 	    
- 	//Get the change total in cents
-        change = (((tender[0] * 100) + tender[1]) - ((bill[0] * 100) + bill[1]));
-    
-        for (unsigned int i = 0; i < sizeof(denominations)/sizeof(*denominations); ++i){
-            calculateChange(&change,denominations[i]);
-        }
+       unsigned int denominations[countWords(myFiles)];
+       unsigned int change = 0;
+       unsigned int tender[2] = {0,0};
+       unsigned int bill[2]  = {0,0};
+       fclose (myFiles);
+       myFiles = fopen("denominations.txt", "r");
+       
+       for(size_t i = 0; i < sizeof(denominations)/sizeof(*denominations); ++i){
+           fscanf(myFiles, "%u", &denominations[i]);
+       }
+       fclose(myFiles);
+       
+       while (!feof(stdin)){
+           if (scanf("%u.%u %u.%u",&tender[0],&tender[1],&bill[0],&bill[1]) == 4) break;
+       }
+       
+       //Get the change total in cents
+       change = (((tender[0] * 100) + tender[1]) - ((bill[0] * 100) + bill[1]));
+       
+       for (size_t i = 0; i < sizeof(denominations)/sizeof(*denominations); ++i){
+           calculateChange(&change,denominations[i]);
+       }
     }
     return 0;
 }
@@ -45,8 +44,9 @@ unsigned int countWords(FILE *f){
    unsigned int count = 0;
    char ch;
    while ((ch = fgetc(f)) != EOF){
-       if (ch == '\n')
+       if (ch == '\n'){
            count++;
+       }
    }
    return ++count;
 }
